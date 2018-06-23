cabaf6db19f30dc89d2a58a14158321fe8e9bb76
LiveSlowSkateFast <jp@jprowan.com>
2018-06-22 20:04:06 -0700

    Check JWT works with config

--- services/checkJwt.js
+++ services/checkJwt.js
@@ -1,17 +1,19 @@
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+import jwt from 'express-jwt'
+import jwksRsa from 'jwks-rsa'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const checkJwt = jwt({
     secret: jwksRsa.expressJwtSecret({
       cache: true,
       rateLimit: true,
       jwksRequestsPerMinute: 5,
-      jwksUri: `https://` + process.env.AUTH0_TENATE +
+      jwksUri: `https://` + process.env.AUTH0_TENANT +
         `.auth0.com/.well-known/jwks.json`
     }),
-
-    audience: process.env.CLIENT_ID,
-    issuer: `https://` + process.env.AUTH0_TENATE + `.auth0.com/`,
+    audience: process.env.AUDIENCE || process.env.DOMAIN,
+    issuer: `https://` + process.env.AUTH0_TENANT + `.auth0.com/`,
     algorithms: ['RS256']
   })
 
