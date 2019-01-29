63d3024c88ed70e72bdb1e311a811886ccd70013
LiveSlowSkateFast <jp@jprowan.com>
2019-01-29 12:16:19 -0800

    moved allowed origins to config

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function (req, res, next) {
-  const allowedOrigins = ['http://localhost:3000', 'https://localtest.com:3000']
+  const allowedOrigins = process.env.ALLOWED_ORIGINS
   const origin = req.headers.origin
   if (allowedOrigins.includes(origin)) res.header("Access-Control-Allow-Origin", origin)
   res.header("Access-Control-Allow-Headers",
