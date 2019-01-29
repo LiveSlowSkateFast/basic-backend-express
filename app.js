946916fc7d8508c7dec0089a0c159d85447e3000
LiveSlowSkateFast <jp@jprowan.com>
2019-01-29 12:12:15 -0800

    added option for multiple CORS origins

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function (_req, res, next) {
-  res.header("Access-Control-Allow-Origin", 'http://localhost:3000')
+app.use(function (req, res, next) {
+  const allowedOrigins = ['http://localhost:3000', 'https://localtest.com:3000']
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) res.header("Access-Control-Allow-Origin", origin)
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
